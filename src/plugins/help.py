d92be9116b0cf5fbc31f24475710ad7dac336a61
JamesM92 <36548759+JamesM92@users.noreply.github.com>
2026-03-15 16:33:00 -0400

    Update and rename core.py to help.py

---
+++ src/plugins/help.py
@@ -0,0 +1,25 @@
+##############################
+
+from commands import register, help_menu, category_help
+
+
+# -------------------------
+# Help Command
+# -------------------------
+
+@register(
+    "help",
+    "Show command categories",
+    category="core",
+    cooldown=30,
+    aliases=["?", "h"]
+)
+def help_cmd(args):
+
+    if not args:
+        return help_menu(1)
+
+    if args[0].isdigit():
+        return help_menu(int(args[0]))
+
+    return category_help(args[0])
