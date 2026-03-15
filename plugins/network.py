252c70aabba365a3d90f3ac5087877d681cba4ef
JamesM92 <36548759+JamesM92@users.noreply.github.com>
2026-03-15 08:42:57 -0400

    Update network.py

--- plugins/network.py
+++ plugins/network.py
@@ -1,14 +1,10 @@
-import time
 from commands import register
-
-@register("whoami", "Show your LXMF address", "network")
-def whoami(args):
-
-    sender = args[-1]
-
-    return f"Your LXMF address:\n{sender}"
+import time
 
 @register("time", "Show node time", "network")
 def node_time(args):
+    return time.strftime("%Y-%m-%d %H:%M:%S")
 
-    return time.strftime("Node time: %Y-%m-%d %H:%M:%S")
+@register("whoami", "Show your LXMF address", "network")
+def whoami(args):
+    return f"Your address:\n{args[-1]}"
