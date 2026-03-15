104eaa9361cd7f2b7ec161769735b321c0dec688
JamesM92 <36548759+JamesM92@users.noreply.github.com>
2026-03-15 08:27:17 -0400

    Update core.py

--- plugins/core.py
+++ plugins/core.py
@@ -1,11 +1,9 @@
 from commands import register, help_menu
 
-
-@register("help", "Show this menu")
+@register("help", "Show help menu", "core")
 def help_cmd(args):
     return help_menu()
 
-
-@register("?", "Show this menu")
+@register("?", "Show help menu", "core")
 def help_short(args):
     return help_menu()
